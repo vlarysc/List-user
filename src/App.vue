8e2a5b6bbcbd29807094132b969a8d1dde6ac2da
JoaoV <joao.vfm18@gmail.com>
2021-10-01 13:33:59 -0300

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1 class="">List User</h1>
+    <div>
+      <button class="border border-black p-2 ">Novo Registro</button>
+    </div>
+    
+    <thead class="flex ">
+      <th class="px-3">Nome</th>
+      <th class="px-3">Telefone</th>
+      <th class="px-3">Email</th>
+      <th class="px-3">Ações</th>
+    </thead>
+    <tbody>
+      <tr> 
+        <td></td>
+      </tr>
+    </tbody>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+
+ }
+}
+</script>
